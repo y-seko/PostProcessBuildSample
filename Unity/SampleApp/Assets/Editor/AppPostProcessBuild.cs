7d7945db7820eca430e854c481dd00619993dd9f
y.seko <y.seko@yseko02-PC.local>
2016-09-12 15:54:53 +0900

    [update]ビルドスクリプトの呼び出しを修正
    ビルドスクリプトに引数を追加

--- Unity/SampleApp/Assets/Editor/AppPostProcessBuild.cs
+++ Unity/SampleApp/Assets/Editor/AppPostProcessBuild.cs
@@ -20,19 +20,51 @@ public class AppPostProcessBuild {
 		RunBuildScript (rootPath);
 	}
 
+	/// <summary>
+	/// ビルドスクリプトを実行する
+	/// </summary>
+	/// <param name="rootPath">Root path.</param>
 	private static void RunBuildScript(string rootPath) {
+		var appName = PlayerSettings.productName;
 		var scriptPath = rootPath + "/Script";
+		var commands = new string[] {
+			"cd " + scriptPath,
+			"sh build.sh " + rootPath + " " + appName,
+		};
 
+		// Terminalを起動してシェルスクリプトを実行する
+		// 直接シェルスクリプトを実行することもできるが進捗状態が出力されないのでTerminal上で実行させる。
 		var process = new System.Diagnostics.Process ();
 		process.StartInfo.UseShellExecute = false;
 		process.StartInfo.RedirectStandardOutput = true;
-		process.StartInfo.FileName = "sh";
-		process.StartInfo.Arguments = scriptPath + "/build.sh " + rootPath;
+		// AppleScriptを実行する
+		process.StartInfo.FileName = "osascript";
+		// [memo]
+		//  LaunchTerminalWithCommand.scpt
+		//  ターミナルを起動してコマンドを実行するAppleScript（何故か直接ターミナルを起動できないので書いた）
+		process.StartInfo.Arguments = scriptPath + "/LaunchTerminalWithCommand.scpt " + CatTarminalCommands(commands);
 		process.Start ();
 
 		// 終了を待つ
 		string strOutput = process.StandardOutput.ReadToEnd (); 
 		process.WaitForExit ();
 		UnityEngine.Debug.Log (strOutput);
+	}
+
+	/// <summary>
+	/// コマンドの文字列を連結
+	/// </summary>
+	/// <returns>The tarminal commands string.</returns>
+	/// <param name="commands">Commands.</param>
+	public static string CatTarminalCommands(string[] commands) {
+		string cmdString = "'";
+		foreach (string command in commands) {
+			if (!commands.GetValue (0).Equals(command)) {
+				cmdString += " && ";
+			}
+			cmdString += command;
+		}
+		cmdString += "'";
+		return cmdString;
 	}
 }
